77ce68d9cc284008b9bb6b11e9166e268fc69d56
Kyle Evans <kevans@FreeBSD.org>
2019-03-04 08:27:51 -0600

    Build the shell version out more completely; more testing required

--- package.sh
+++ package.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# \o/!
+set -o pipefail
+
 SCRIPTDIR=$(dirname $(realpath ${0}))
 subrscript=${SCRIPTDIR}/package.subr
 localscript=${SCRIPTDIR}/local.sh
+
+WORKDIR=${SCRIPTDIR}/work
+SCRATCHDIR=$(mktemp -d /tmp/fpbuild.XXXXX)
+trap "rm -rf ${SCRATCHDIR}" exit
 
 . ${subrscript}
 [ -f "${localscript}" ] && . ${localscript}
@@ -28,6 +35,7 @@ CONFPATTERN="${CONFPREFIX}(.+)"
 : ${KERNCONF:=GENERIC}
 
 # Utilities
+: ${GIT:=$(which git)}
 : ${LN:=ln}
 : ${FIND:=find}
 : ${DIFF:=diff -q}
@@ -40,6 +48,26 @@ CONFPATTERN="${CONFPREFIX}(.+)"
 [ -f ${MAKE_CONF} ] || MAKE_CONF=/dev/null
 [ -f ${SRC_CONF} ] || SRC_CONF=/dev/null
 [ -f ${SRC_ENV_CONF} ] || SRC_ENV_CONF=/dev/null
+
+args=$(getopt s $*)
+if [ $? -ne 0 ]; then
+	echo "Usage: $0 [-s]"
+	exit 1
+fi
+
+set -- ${args}
+while :; do
+	case "$1" in
+		-s)
+			SILENT=yes
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+	esac
+done
 
 ARCH_DIRS=$(get_arch_dirs)
 
@@ -63,7 +91,69 @@ for srctop in ${SRCTOP}; do
 		1>&2 echo "Invalid arch detected (in list ${archs})"
 		exit 1
 	fi
+	# Stash them away for now
+	echo ${archs} > ${SCRATCHDIR}/arch.$(canonicalize_srctop ${srctop})
 done
 
-args=$(build_make_args)
+builddate=$(get_tag_date)
+# Start building
+for srctop in ${SRCTOP}; do
+	csrctop=$(canonicalize_srctop "${srctop}")
+	archlist=$(cat ${SCRATCHDIR}/arch.${csrctop})
+	srctop=$(path_from_srctop "${SRCTOP}")
+	# First, we tag
+	if [ -z "${NOTAG}" ] && [ ! -z "${GIT}" ]; then
+		for arch in ${archlist}; do
+			buildtag=$(get_machine_arch "${arch}")
+			env GIT_DIR=${srctop}/.git git tag "build/${buildtag}/${builddate}"
+		done
+	fi
 
+	# Next, we sync up this srctop's configs
+	for arch in ${archlist}; do
+		confdest=${srctop}/sys/$(get_machine "${arch}")/conf
+		for cfgfile in $(${FIND} "${confdest}" -lname "${CONFTOP}/*"); do
+			[ -e ${cfgfile} ] || ${RM} ${cfgfile}
+		done
+
+		configs=$(get_arch_configs "${arch}")
+		for cfgfile in ${configs}; do
+			cname=$(get_config_name "${cfgfile}")
+			tgtcfg="${confdest}/${cname}"
+			if [ -e "${tgtcfg}" ]; then
+				${DIFF} "${tgtcfg}" "${cfgfile}" || ${RM} "${tgtcfg}"
+			fi
+
+			[ ! -e "${tgtcfg}" ] && ${LN} -s "${cfgfile}" "${tgtcfg}"
+		done
+	done
+
+	repodirs=""
+	# Let's start building!
+	for archspec in ${archlist}; do
+			WLOG=${WRKDIR}/build-world-${csrctop}-${archspec}.log
+			KLOG=${WRKDIR}/build-kernel-${csrctop}-${archspec}.log
+			PLOG=${WRKDIR}/packages-${csrctop}-${archspec}.log
+
+			${RM} ${WLOG}
+			${RM} ${KLOG}
+			${RM} ${PLOG}
+
+			args=$(build_make_args "${srctop}" "${archspec}")
+			KERNCONF=$(_get_kernconf_set "${archspec}")
+			_build_target buildworld "${WLOG}"
+			_build_target buildkernel "${KLOG}"
+			_build_target packages "${PLOG}"
+			repodirs="${repodirs} ${objtop}${srctop}/repo"
+	done
+
+	# Do /repo maintenance
+	[ ! -d "${PKGTOP}/repo" ] && ${MKDIR} "${PKGTOP}/repo"
+
+	for repodir in ${repodirs}; do
+		for abidir in $(${FIND} ${repodir} -type d -d 1); do
+			[ ! -e ${PKGTOP}/repo/$(basename ${abidir}) ] && \
+				${LN} -s ${abidir} ${PKGTOP}/repo
+		done
+	done
+done
